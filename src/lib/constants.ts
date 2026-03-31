4552d1e81d553f9249e24f2646235e1ae7d704cf
Mahmoud Elamrosy <74033364+melamrosy72@users.noreply.github.com>
2026-03-31 16:34:45 +0200

    Update docker-compose.yml network name and change SIGNIN_URL to use '/login'

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,4 +2,4 @@ export const ACADEMY_APP_URL =
   process.env.NEXT_PUBLIC_ACADEMY_URL || "https://app.omnexa-learn.com";
 
 export const SIGNUP_URL = `${ACADEMY_APP_URL}/signup`;
-export const SIGNIN_URL = `${ACADEMY_APP_URL}/signin`;
+export const SIGNIN_URL = `${ACADEMY_APP_URL}/login`;
