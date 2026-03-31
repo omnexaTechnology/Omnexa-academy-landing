67fda4dd9dc15c00ac9d32dc3a16e296ef8fa29a
Mahmoud Elamrosy <74033364+melamrosy72@users.noreply.github.com>
2026-03-31 15:34:21 +0200

    deployment

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,6 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = { output: "standalone" };
 
 export default withNextIntl(nextConfig);
